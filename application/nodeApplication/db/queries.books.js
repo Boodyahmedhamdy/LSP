6c7d9198a2aa48475abe8c09ec1df182c1728b63
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2023-04-20 16:01:10 +0200

    all methods to get book with it's attributes

--- application/nodeApplication/db/queries.books.js
+++ application/nodeApplication/db/queries.books.js
@@ -1,9 +1,50 @@
 const promisedQuery = require("./promisedQuery")
 
-// BOOKS
+// BOOKS Getting
 async function getAllBooks() {
     return promisedQuery.createPromisedQuery(
         "select * from books"
+    )
+}
+
+async function getBookById(id) {
+    return promisedQuery.createPromisedQuery(
+        "select * from books where id = ?", 
+        [id]
+    )
+}
+
+async function getBooksByName(name) {
+    return promisedQuery.createPromisedQuery(
+        "select * from books where name like '%?%'", 
+        [name]
+    )
+}
+
+async function getBooksByAuthorName(authorName) {
+    return promisedQuery.createPromisedQuery(
+        "select * from books where author_name like '%?%'", 
+        [authorName]
+    )
+}
+
+async function getBooksByPrice(price) {
+    return promisedQuery.createPromisedQuery(
+        "select * from books where price = ?", 
+        [price]
+    )
+}
+async function getBooksWithPriceLessThanOrEquals(price) {
+    return promisedQuery.createPromisedQuery(
+        "select * from books where price <= ?", 
+        [price]
+    )
+}
+
+async function getBooksWithPriceGreaterThanOrEquals(price) {
+    return promisedQuery.createPromisedQuery(
+        "select * from books where price >= ?", 
+        [price]
     )
 }
 
